dbc7b5101ffa71888a517bf50940dac6b8027ab8
barba-rasta <barbanesta@gmail.com>
2026-02-27 03:33:08 -0600

    Update requisitos.sh to install packages with pip3

---
+++ requisitos.sh
@@ -0,0 +1,7 @@
+pip3 install requests pyfiglet colorama pycurl
+# Herramienta de traducción
+pkg install -y translate-shell
+
+# Para limpiar pantalla y manipular terminal
+pkg install -y  ncurses-utils
+
